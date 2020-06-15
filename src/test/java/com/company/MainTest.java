0f307d4c287469deb878bc0248e1b12cb66c0140
DimetriuS92 <Fakestyle92@gmail.com>
2020-06-16 00:29:52 +0300

    Initial commit

---
+++ src/test/java/com/company/MainTest.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void main() {
+    }
+
+    @Test
+    public void isValidInput() {
+
+    }
+
+    @Test
+    public void nextPlayer() {
+
+    }
+
+    @Test
+    public void isGameEnd() {
+    }
+
+    @Test
+    public void getXY() {
+    }
+
+    @Test
+    public void getX() {
+    }
+
+    @Test
+    public void putX() {
+    }
+
+    @Test
+    public void showPole() {
+    }
+}
